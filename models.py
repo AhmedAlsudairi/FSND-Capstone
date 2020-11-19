55bdcc14374585ed855ec2c62f87815e74962202
AhmedAlsudairi <AhmedAlsudairi@users.noreply.github.com>
2020-11-19 07:04:40 +0300

    creating Movie model

--- models.py
+++ models.py
@@ -1,1 +1,36 @@
+from sqlalchemy import Column, String, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
+database_path = os.environ['DATABASE_URL']
 
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Movie(db.Model):  
+  __tablename__ = 'Movie'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release_date = Column(String)
+
+  def __init__(self, title, release_date=""):
+    self.title = title
+    self.release_date = release_date
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release_date': self.release_date}
