f9af3dbc96574090197be1ed4b299855ca4da9dd
AhmedAlsudairi <AhmedAlsudairi@users.noreply.github.com>
2020-11-19 23:44:09 +0300

    creating migration for database

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-
-DATABASE_URL=postgres://postgres:admin@localhost:5432/casting_agency
+set DATABASE_URL="postgres://postgres:admin@localhost:5432/casting_agency"
+$env:DATABASE_URL = "postgres://postgres:admin@localhost:5432/casting_agency"
