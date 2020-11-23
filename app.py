fd8e1d6a8037452ef53c3788f6a3a86c19a28d96
AhmedAlsudairi <AhmedAlsudairi@users.noreply.github.com>
2020-11-23 19:41:20 +0300

    implementing POST /actors endpoint

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db, Movie, Actor, Role
 
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
-  setup_db(app)
-  return app
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
+    setup_db(app)
+    return app
+
 
 app = create_app()
+
 
 @app.route('/actors', methods=['GET'])
 def get_actors():
@@ -20,11 +23,33 @@ def get_actors():
         formated_actors = [actor.format() for actor in actors]
         print(formated_actors)
         return jsonify({
-          "success" : True,
-          "actors" : formated_actors
+            "success": True,
+            "actors": formated_actors
         })
     except:
-        abort(422)  
+        abort(422)
+
+
+@app.route('/actors', methods=['POST'])
+def post_actor():
+    try:
+        body = request.get_json()
+
+        name = body['name']
+        age = body['age']
+        gender = body['gender']
+        actor = Actor(name=name, gender=gender, age=age)
+        print(actor.name)
+        actor.insert()
+
+        return jsonify({
+            "success": True,
+            "created": actor.id,
+            "total_actors": len(Actor.query.all())
+        })
+    except:
+        abort(400)
+
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
@@ -33,11 +58,12 @@ def get_movies():
         formated_movies = [movie.format() for movie in movies]
         print(formated_movies)
         return jsonify({
-          "success" : True,
-          "movies" : formated_movies
+            "success": True,
+            "movies": formated_movies
         })
     except:
-        abort(422)          
+        abort(422)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
